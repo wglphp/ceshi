30ee1885d9296de98009c67ce64e34a18787461e
guangli.wang@haitaotiantang.com <1169118419@qq.com>
2017-01-11 23:48:11 +0800

    tianjiaweibo

---
+++ weibo/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '2483417349' );
+define( "WB_SKEY" , 'a6b447f3a8e626ee15af2f5db7b5f439' );
+define( "WB_CALLBACK_URL" , 'http://localhost/weibo/callback.php' );
